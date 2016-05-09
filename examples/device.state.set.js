5922404231545685bc4a2d569919e3cd9727f464
Harrison H. Jones <harrison@hhj.me>
2016-05-08 23:51:00 -0700

    Added non-MQTT device setting example

--- examples/device.state.set.js
+++ examples/device.state.set.js
@@ -2,6 +2,7 @@
 // process.env.NOMIKU_EMAIL - Your Nomiku/Tender email
 // process.env.NOMIKU_PASSWORD - Your Nomiku/Tender password
 // process.env.NOMIKU_HARDWARE_ID - The device hardware id of the device you want to control
+// process.env.NOMIKU_DEVICE_ID - The device hardware id of the device you want to control
 
 // Import the Nomiku library
 // var nom = require('nomiku-js');  // Uncomment if you installed nomiku-js with `npm install nomiku-js`
@@ -27,7 +28,7 @@ nom.auth(process.env.NOMIKU_EMAIL, process.env.NOMIKU_PASSWORD, function(error){
     console.log("[INFO]\t\tThe current access token is", nom.getToken());
 
     // Set the device's state using MQTT (the only supported method at the moment)
-    nom.setMQTT(process.env.NOMIKU_HARDWARE_ID, 'setpoint', '63', function(error, deviceInfo)
+    /*nom.setMQTT(process.env.NOMIKU_HARDWARE_ID, 'setpoint', '63', function(error, deviceInfo)
     {
         if(error)
         {
@@ -39,5 +40,19 @@ nom.auth(process.env.NOMIKU_EMAIL, process.env.NOMIKU_PASSWORD, function(error){
             console.log('[INFO] Device state set.');
             console.log(deviceInfo);
         }
-    });    
+    });    */
+
+    nom.setDeviceState(process.env.NOMIKU_DEVICE_ID, {state: nom.STATE_OFF, setpoint: nom.FtoC(135)}, function(error, deviceInfo)
+    {
+        if(error)
+        {
+            console.log("[ERROR] Failed to set the device's state. Error =", error);
+            return;
+        }
+        else
+        {
+            console.log('[INFO] Device state set.');
+            console.log(deviceInfo);
+        }
+    }); 
 });
